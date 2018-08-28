8407d39b76e0c4ae72088cea268603a311e5c54e
lukiguki <31853688+lukiguki@users.noreply.github.com>
2018-08-28 14:34:24 +0200

    Installerat Xampp och provat med vår första PHP kod

---
+++ htdocs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Första php-koden</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<p><?php echo "Första php-koden"; ?></p>
+<p> Klockan är <?php echo date("H:i:s"); ?></p>
+</body>
+</html>
